eb535e0aeeca8a683a6eb68000a0902968847d19
Aaron Armbruster <armbrust@cern.ch>
2017-07-09 14:57:25 +0200

    add multiple units

---
+++ Global.py
@@ -0,0 +1,4 @@
+
+
+colors = ['Blue', 'Green', 'Orange', 'Purple', 'Red', 'Tan', 'White', 'Yellow']
+ships = ['Carrier', 'Cruiser', 'Destroyer', 'Dreadnaught', 'Fighter', 'Ground Force', 'PDS', 'Space Dock', 'War Sun']
